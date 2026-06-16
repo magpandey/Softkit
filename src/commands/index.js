95d66555af3310e5f3599f2bbc53ad127c94fac4
= <pandeyvinayak589@gmail.com>
2026-06-16 14:33:04 +0530

    add listFilesAndDirectory function; update commands to include give-info

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,12 +2,14 @@ const helpFunction = require("./help.js");
 const exitFunction = require("./exit.js");
 const whereFunction = require("./where.js");
 const goFunction = require("./go.js");
+const listFilesAndDirectory = require("./list.js");
 
 const commands = {
     "help" : helpFunction,
     "exit" : exitFunction,
     "loca-where" : whereFunction,
-    "get-into" : goFunction
+    "get-into" : goFunction,
+    "give-info" : listFilesAndDirectory
 }
 
 module.exports = commands
---
+++ src/commands/list.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const {getCurrentDirectory} = require("../core/workspace.js");
+const path = require("path");
+
+function listFilesAndDirectory(input){
+    const dir = getCurrentDirectory();
+    try {
+        const items = fs.readdirSync(dir);
+        if(items.length === 0){
+            console.log(`${dir} is empty`);
+            return;
+        }
+        console.log(`\nContents of ${dir}:\n`);
+
+        items.forEach(e => {
+            const fullpath = path.join(dir,e);
+            const isDir = fs.statSync(fullpath).isDirectory();
+
+             if (isDir) {
+                console.log(`  [DIR]  ${e}`);
+            } else {
+                console.log(`  [FILE] ${e}`);
+            } 
+        });
+        console.log("");
+    } catch (error) {
+        console.log(`Could'nt read the directory : ${error.message}`)
+    }
+}
+module.exports = listFilesAndDirectory
