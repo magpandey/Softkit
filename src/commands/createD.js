376cd082cc9d7b1c7b3cf2128b4e16a671a6f705
= <pandeyvinayak589@gmail.com>
2026-06-19 16:17:45 +0530

    fix: correct console log typo and improve folder creation logic handling

--- src/commands/createD.js
+++ src/commands/createD.js
@@ -6,14 +6,16 @@ function commandCreateFolder(args,rl){
         cosole.log(`Please provide folder name`);
         return;
     }
-
+    const shouldEnter = args[1] === "--enter";
     const {success,message} = createFolder(args[0]);
-    if(success){
+    if(success && shouldEnter){
          console.log(message);
          const moved = changeDirectory(args[0]);
          if(moved.success){
             console.log(`Moved to : ${moved.path}`)
          }
+    }else if(success && !shouldEnter){
+        console.log(message);
     }else{
         console.log(message);
     }
