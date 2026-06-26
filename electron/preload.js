95fb3fa6b7adb80343ea7049cd3d17e3d8f0d767
= <pandeyvinayak589@gmail.com>
2026-06-26 18:09:57 +0530

    feat: enhance UI in index.html and implement IPC communication in preload.js; refactor logger functions for better output handling

--- electron/preload.js
+++ electron/preload.js
@@ -1,1 +1,13 @@
-console.log("preload loaded");
+
+const {contextBridge,ipcRenderer} = require("electron")
+
+contextBridge.exposeInMainWorld('softkit',{
+    sendCommand: (input) => {
+        ipcRenderer.send('command',input)
+    },
+    onOutput: (callback) => {
+        ipcRenderer.on('output',(event,data) => {
+            callback(data);
+        })
+    }
+})
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,24 +1,45 @@
-// src/utils/logger.js
+
+
+let ipcSender = null;
+
+function setIPCMode(senderFn) {
+    ipcSender = senderFn;
+}
+
+function print(prefix, colorCode, message) {
+    if (ipcSender) {
+        ipcSender(`${prefix} ${message}`);
+    } else {
+        console.log(`${colorCode}${prefix}\x1b[0m ${message}`);
+    }
+}
 
 function success(message) {
-    console.log("\x1b[32m[SUCCESS]\x1b[0m " + message);
+    print('[SUCCESS]', '\x1b[32m', message);
 }
 
 function error(message) {
-    console.log("\x1b[31m[ERROR]\x1b[0m " + message);
+    print('[ERROR]', '\x1b[31m', message);
 }
 
 function warn(message) {
-    console.log("\x1b[33m[WARN]\x1b[0m " + message);
+    print('[WARN]', '\x1b[33m', message);
 }
 
 function info(message) {
-    console.log("\x1b[36m[INFO]\x1b[0m " + message);
+    print('[INFO]', '\x1b[36m', message);
 }
+
+function newline() {
+    if (ipcSender) {
+        ipcSender('');
+    } else {
+        console.log('');
+    }
+}
+
 function question(message) {
-    return "\x1b[33m" + message + "\x1b[0m";
+    return '\x1b[33m' + message + '\x1b[0m';
 }
-function newline(){
-    console.log("")
-}
-module.exports = { success, error, warn, info,question,newline };
+
+module.exports = { success, error, warn, info, newline, question, setIPCMode };
